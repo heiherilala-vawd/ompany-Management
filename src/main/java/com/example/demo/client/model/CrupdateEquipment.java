b7a312d5477595d7a03fd6ceb8eddd8a0039d294
heiherilala-vawd <hei.herilala@gmail.com>
2026-04-29 19:46:07 +0300

    feat: add travel

--- src/main/java/com/example/demo/client/model/CrupdateEquipment.java
+++ src/main/java/com/example/demo/client/model/CrupdateEquipment.java
@@ -40,7 +40,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
   CrupdateEquipment.JSON_PROPERTY_STORAGE_NUMBER,
   CrupdateEquipment.JSON_PROPERTY_COMMENT
 })
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-29T14:25:24.972835022+03:00[Indian/Antananarivo]", comments = "Generator version: 7.6.0")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-29T15:42:01.292627554+03:00[Indian/Antananarivo]", comments = "Generator version: 7.6.0")
 public class CrupdateEquipment {
   public static final String JSON_PROPERTY_ID = "id";
   private String id;
--- src/main/java/com/example/demo/client/model/TravelPeople.java
+++ src/main/java/com/example/demo/client/model/TravelPeople.java
@@ -44,7 +44,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
   TravelPeople.JSON_PROPERTY_UPDATED_BY,
   TravelPeople.JSON_PROPERTY_COMMENT
 })
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-29T14:25:24.972835022+03:00[Indian/Antananarivo]", comments = "Generator version: 7.6.0")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-29T15:42:01.292627554+03:00[Indian/Antananarivo]", comments = "Generator version: 7.6.0")
 public class TravelPeople {
   public static final String JSON_PROPERTY_ID = "id";
   private String id;
