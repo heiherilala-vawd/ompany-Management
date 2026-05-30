c40b1090ca2df209e6c5aedd5ab841e45c3e38db
herilala <hei.herilala@gmail.com>
2026-05-30 17:49:17 +0300

    test: delet sonar

--- src/main/java/com/example/demo/client/model/AuditFields.java
+++ src/main/java/com/example/demo/client/model/AuditFields.java
@@ -39,7 +39,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
   AuditFields.JSON_PROPERTY_UPDATED_BY,
   AuditFields.JSON_PROPERTY_COMMENT
 })
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-05-26T13:42:36.425111812+03:00[Indian/Antananarivo]", comments = "Generator version: 7.6.0")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-05-30T17:19:15.606578257+03:00[Indian/Antananarivo]", comments = "Generator version: 7.6.0")
 public class AuditFields {
   public static final String JSON_PROPERTY_CREATED_AT = "created_at";
   private java.time.Instant createdAt;
--- src/main/java/com/example/demo/client/model/CrupdateTask.java
+++ src/main/java/com/example/demo/client/model/CrupdateTask.java
@@ -45,7 +45,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
   CrupdateTask.JSON_PROPERTY_COMPANY_ID,
   CrupdateTask.JSON_PROPERTY_ASSIGNED_USER_IDS
 })
-@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-05-26T13:42:36.425111812+03:00[Indian/Antananarivo]", comments = "Generator version: 7.6.0")
+@jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-05-30T17:19:15.606578257+03:00[Indian/Antananarivo]", comments = "Generator version: 7.6.0")
 public class CrupdateTask {
   public static final String JSON_PROPERTY_ID = "id";
   private String id;
